cb9e489a8d79472bff1a046df9300b7650ad46d3
CarpeSwag <james@garbagnati.com>
2017-10-07 21:28:50 -0700

    Separated out the Poem from the component.

---
+++ components/Poem.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import {
+	AppRegistry,
+	asset,
+	Pano,
+	Image,
+	Text,
+	View,
+} from 'react-vr';
+
+export default class Poem extends React.Component {
+	render() {
+		return (
+			<Image
+				source={asset(this.props.src)}
+				style={{
+					position: 'absolute',
+					width: this.props.width,
+					height: this.props.height,
+					transform: [
+						{rotateY: -45},
+						{translate: [
+							this.props.x,
+							this.props.y,
+							this.props.z
+						]}
+					],
+				}}/>
+		);
+	}
+};
+
+AppRegistry.registerComponent('Poem', () => Poem);
--- index.vr.js
+++ index.vr.js
@@ -1,52 +1,37 @@
 import React from 'react';
 import {
-  AppRegistry,
-  asset,
-  Pano,
-  Image,
-  Text,
-  View,
+	AppRegistry,
+	asset,
+	Pano,
+	Image,
+	Text,
+	View,
 } from 'react-vr';
+import Poem from './components/Poem';
 
 export default class repo2 extends React.Component {
-  render() {
-    return (
-      <View>
-        <Pano source={[
-          asset('img_left.jpg'),
-          asset('img_right.jpg'),
-          asset('img_top.jpg'),
-          asset('img_bot.jpg'),
-          asset('img_right_far.jpg'),
-          asset('img_center.jpg'),
-        ]} />
-        <Image 
-          source={asset('ghazal 1.png')}
-          style={{
-            position: 'absolute',
-            width: 800,
-            height: 1280,
-            transform: [
-              {rotateY: -45},
-              {translate: [-100, 655, 0]},
-              {scale: 0.25}
-            ],
-          }}/>
-          <Image 
-          source={asset('ghazal 1-Eng.png')}
-          style={{
-            position: 'absolute',
-            width: 600,
-            height: 500,
-            transform: [
-              {rotateY: -45},
-              {translate: [-300, 275, -50]},
-              {scale: 0.5}
-            ],
-          }}/>
-      </View>
-    );
-  }
+	render() {
+		return (
+			<View>
+				<Pano source={[
+					asset('img_left.jpg'),
+					asset('img_right.jpg'),
+					asset('img_top.jpg'),
+					asset('img_bot.jpg'),
+					asset('img_right_far.jpg'),
+					asset('img_center.jpg'),
+				]} />
+				<Poem
+					src='ghazal 1-Eng.png'
+					width={300} height={250}
+					x={-200} y={160} z={-200} />
+				<Poem
+					src='ghazal 1.png'
+					width={200} height={320}
+					x={115} y={182} z={-250} />
+			</View>
+		);
+	}
 };
 
 AppRegistry.registerComponent('repo2', () => repo2);
